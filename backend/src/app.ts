0bb16f03a44d356bac75ed1268b531e237383d70
Cayman <93142107+caymanwjeffers@users.noreply.github.com>
2024-11-17 14:03:34 -0500

    configure fly backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,20 @@
-// boiler plate for app file in basic node server
+//@ts-nocheck
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
 import apiRouter from "./api.router";
 
-dotenv.config({
-  path: path.resolve() + "/.env",
-});
-
 const app = express();
 app.use(cors());
+app.use(express.json());
+
 app.use("/api", apiRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
+
+export default app;
